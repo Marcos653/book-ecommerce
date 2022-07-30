0e3710e8f9b2b5cdf58a4dff69e05f1a0a95ee80
Your Name <viniciustatuta@gmail.com>
2022-07-30 12:56:23 -0300

    feat: deploy by heroku

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import Group as DjangoGroup
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
 
-admin.site.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'name', 'is_admin',)
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email',
+                           'password',
+                           'name',
+                           'cpf',
+                           'type_user',
+                           'image_user',
+                           'sex',
+                           'birth_date',
+                           'phone',
+                           'balance',
+                           'active',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2',)}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
